b8b6ec58100977590500b26b65bd48a948bda0ca
Kairat <productgsm@gmail.com>
2023-09-01 22:30:47 +0500

    delete function adding

--- database.py
+++ database.py
@@ -12,3 +12,13 @@ def update_new_obj(data):
     old_data.update(data)
     with open("db.json", "w") as json_file:
         json.dump(old_data, json_file, indent=4)
+
+
+def delete_obj(key):
+    old_data = load_db()
+    if key in old_data:
+        del old_data[key]
+        with open('db.json', 'w') as json_file:
+            json.dump(old_data, json_file, indent=4)
+    else:
+        print(f"The key '{key}' does not exist in the JSON data.")
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from telebot.types import Message
 
 from config import BOT_TOKEN, USER_ID, YOUTUBE_RSS_URL, BOT_DEBUG
-from database import update_new_obj
+from database import update_new_obj, delete_obj
 from utils import validate_channel, extract_channel_id, get_all_urls_from_rss, get_all_new_links
 
 # Создание объекта бота
@@ -23,26 +23,47 @@ def my_interval_job():
 
 
 @bot.message_handler()
-def handle_specific_user_message(message: Message):
+def handle_specific_user_message(m: Message):
     try:
-        if message.from_user.id == int(USER_ID):
-            if message.text == "/start":
-                bot.send_message(message.from_user.id, "Я активно работаю :)")
+        if m.from_user.id == int(USER_ID):
+            if m.text == "/start":
+                bot.send_message(m.from_user.id, "Я активно работаю :)")
+            elif m.text.startswith("/add ") or m.text.startswith("/del "):
+                try:
+                    command, url = m.text.split()
+                    if command == "/add":
+                        exists, channel_name = validate_channel(url)
+                        if exists:
+                            channel_id, _username = extract_channel_id(url)
+                            movie_urls: list = get_all_urls_from_rss(YOUTUBE_RSS_URL + channel_id)
+                            update_new_obj({channel_id: movie_urls})
+                            bot.send_message(m.from_user.id, "✅ Канал успешно добавлен: " + channel_name)
+                        else:
+                            bot.send_message(m.from_user.id,
+                                             "Что-то неправильно отправили. \n"
+                                             "Пример ссылки: https://www.youtube.com/channel/UCTgEgW843fSrCe_vPo4DHIA",
+                                             disable_web_page_preview=True)
+                    else:
+                        exists, channel_name = validate_channel(url)
+                        if exists:
+                            channel_id, _username = extract_channel_id(url)
+                            delete_obj(channel_id)
+                            bot.send_message(m.from_user.id, "❌ Канал успешно удален: " + channel_name)
+                        else:
+                            bot.send_message(m.from_user.id,
+                                             "Что-то неправильно отправили. \n"
+                                             "Пример ссылки: https://www.youtube.com/channel/UCTgEgW843fSrCe_vPo4DHIA",
+                                             disable_web_page_preview=True)
+
+                except:
+                    bot.send_message(m.from_user.id, "Отправленный текст некорректен")
             else:
-                text = message.text
-                exists, channel_name = validate_channel(text)
-                if exists:
-                    channel_id, _username = extract_channel_id(text)
-                    movie_urls: list = get_all_urls_from_rss(YOUTUBE_RSS_URL + channel_id)
-                    update_new_obj({channel_id: movie_urls})
-                    bot.send_message(message.from_user.id, "Канал успешно добавлен: " + channel_name)
-                else:
-                    bot.send_message(message.from_user.id,
-                                     "Что-то неправильно отправили. \n"
-                                     "Пример ссылки: https://www.youtube.com/channel/UCTgEgW843fSrCe_vPo4DHIA",
-                                     disable_web_page_preview=True)
+                bot.send_message(m.from_user.id, "Для того, чтобы добавить или удалить канал отправьте,\n "
+                                                 "/del URL\n"
+                                                 "/add URL")
+
+
     except Exception as e:
-        # Отправляем ошибку на Sentry
         if BOT_DEBUG is False:
             import sentry_sdk
             sentry_sdk.capture_exception(e)
